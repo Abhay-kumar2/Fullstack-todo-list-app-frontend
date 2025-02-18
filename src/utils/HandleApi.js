0e236c6d81d5075478cb112f55ee4c65dcf33623
Abhay-kumar2 <kr.abhay112@gmail.com>
2025-02-18 20:39:23 +0530

    api configuration

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:5000"
+const baseUrl = "https://fullstack-todo-list-app-backend-uu9t.onrender.com"
 
 const getAllToDo = (setToDo) => {
     axios
